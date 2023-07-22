a56b661d267d17642702c0ddfa9ab9565d0b0c06
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2023-07-22 16:20:20 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print('Everton Luis')
+print('"Everton Luis"')
+
+
+# Aspas duplas
+print("Everton Luis")
+
+# Escape 
+print("Everton \"Luis\"")
+
+# r
+print(r"Everton \"Luis\"")
